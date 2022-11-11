076461f27c0b4605b7027b9e03c42af992b2b62f
Frédéric TRAN <frederictran@hotmail.fr>
2022-11-11 16:04:58 +0100

    Revert "Infinite runtime"
    This reverts commit 76ae7815df13cd47ef29ea7820457e30a8ed5db5.

--- main.py
+++ main.py
@@ -35,7 +35,9 @@ if __name__ == '__main__':
     (max_total, ) = cursor.fetchone()
 
     max_daily = 0
-    while connection.open:
+    start_time = time.time()
+    current_time = time.time()
+    while (current_time - start_time) <= 7200 and connection.open:
         instant, daily, total = 0, 0, 0
         if 4 <= int(datetime.now(timezone.utc).strftime('%H')) < 20:
             timestamp = datetime.now(timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
@@ -56,6 +58,7 @@ if __name__ == '__main__':
         else:
             time.sleep(600)
             max_daily = 0
+        current_time = time.time()
 
     cursor.close()
     connection.close()
