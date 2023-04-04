56520d71075900c7e107a3ce17549e8afbd179d8
Frédéric TRAN <frederictran@hotmail.fr>
2023-04-04 16:37:33 +0200

    fix: Cleaner scrapper

--- solar.py
+++ solar.py
@@ -19,12 +19,22 @@ class Solar:
 
     def scraper(self):
         try:  # Password here
-            soup = Bs(requests.get(self.ADDRESS, timeout=10, auth=(self.CFG["SOLAR"]["user"], self.CFG["SOLAR"]["password"])).content, "lxml")
-            data = soup.find_all('script')[1].text.strip().split(";")
-            return data
+            soup = Bs(requests.get(self.ADDRESS, timeout=10,
+                                   auth=(self.CFG["SOLAR"]["user"], self.CFG["SOLAR"]["password"])).content, "lxml")
+            escapes = ''.join([chr(char) for char in range(1, 32)])
+            soup.find_all('script')[1].text.translate(str.maketrans('', '', escapes))
+            data = soup.find_all('script')[1].text.translate(str.maketrans('', '', escapes)) \
+                .replace("\"", "") \
+                .replace("var", "") \
+                .replace(" ", "") \
+                .split(";")
+            output = data[5:8]
+            return float(output[0].split("=")[1]), float(output[1].split("=")[1]), float(output[2].split("=")[1])
         except (ConnectionError, requests.exceptions.ConnectionError, socket.error, socket.gaierror,
                 socket.herror, socket.timeout):
             raise ConnectionError()
+        except ValueError:
+            raise ValueError()
 
     def commit_entry_instant_inverter_power(self, cursor, timestamp, power):
         temp_inverter_instant = self.INVERTER_NO + "_instant"
@@ -35,7 +45,7 @@ class Solar:
         temp_inverter_total = self.INVERTER_NO + "_total"
         cursor.execute(f"SELECT power FROM {temp_inverter_total} ORDER BY timestamp DESC LIMIT 1;")
         print(timestamp + " - " + self.INVERTER_NO + " - Total asked")
-        (temp_total, ) = cursor.fetchone()
+        (temp_total,) = cursor.fetchone()
 
         if temp_total < total:
             cursor.execute(f"INSERT INTO {temp_inverter_total} (timestamp, power) VALUES (?,?)", (timestamp, total))
@@ -44,11 +54,8 @@ class Solar:
     def get_information(self, cursor, timestamp):
         instant_inverter_power, temp_daily, temp_total = 0, 0, 0
         try:
-            data = self.scraper()
-            self.latest_power = int(data[5][23:-1])
-            instant_inverter_power = int(data[5][23:-1])
-            temp_daily = float(data[6][25:-1])
-            temp_total = float(data[7][25:-1])
+            self.latest_power, temp_daily, temp_total = self.scraper()
+            instant_inverter_power = self.latest_power
             self.commit_entry_total_inverter(cursor, timestamp, temp_total)
         except (ConnectionError, ValueError):
             if 4 <= int(datetime.now(timezone.utc).strftime('%H')) < 20:
